168b07097e4895f6835906eec1000c028e0a4bb1
sagar praveen <168666276+sagar00praveen@users.noreply.github.com>
2025-10-28 11:50:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,15 @@ connectDB();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"], 
+    origin: [
+      "http://localhost:5173", 
+      "https://mern-student-teacher-frontend.onrender.com"
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
   })
 );
+
 app.use(express.json());
 
 
