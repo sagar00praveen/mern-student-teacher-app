ebf06498aa0a584242dfa3dc215778abe81bb264
sagar praveen <168666276+sagar00praveen@users.noreply.github.com>
2025-10-28 20:40:03 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,8 @@ connectDB();
 app.use(
   cors({
     origin: ["http://localhost:5173",
-      "https://mern-student-teacher-frontend.onrender.com"
+      "https://mern-student-teacher-frontend.onrender.com",
+        "https://mern-student-teacher-app-1.onrender.com"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
